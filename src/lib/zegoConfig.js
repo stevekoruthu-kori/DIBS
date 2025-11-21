3b944b84df2b00ea96a64f7440e4ebd2e3289a99
KenJose03 <kenjose03@gmail.com>
2025-11-21 18:45:39 +0530

    newtokens

--- src/lib/zegoConfig.js
+++ src/lib/zegoConfig.js
@@ -13,9 +13,8 @@ export const HOST_STREAM_ID = "dibs-host-stream-1";
 
 // ⚠️ TOKENS MUST BE FETCHED FROM BACKEND IN PRODUCTION.
 // For temporary manual testing, you can place static tokens in env vars.
-export const HOST_TOKEN = "04AAAAAGkgC7oADGcNB39oxkpRUPM7vgC0swucofoiLZQV5sZus1tvAww/ZUkEdfvshnAYmAed/troEaRrZkycSC0h5LZZr8GoVyoDVyrkU4+4TWEGcwZf/quPU+uWGeO+yJ5q1vknwkFSDVsBf3i8nsAA+takyR4r7uvTKERrx9KmiSTtUnTlKENMR/mL1M0rUuSz/xmm5VlN4YUs2ww++Qwm2crznZgmd41oQEnmV4WElpb1PJaWZ1s4hHc4Th1Gd2YTJRqeUpNWZjW0AQ==";
-export const VIEWER_TOKEN = "04AAAAAGkgC9UADB0eQ9KMt2wvjOX/0wC2wfBYcLKzy2ml6LWyAxsAwFWsxbW+xYyn6XfrG+7dPXIFrW6L34IXyNqiYFy+pmG07o+Heo6LmeSiIE5ep4E2q0KPDe8mN+d9+ZJ+c47uk7BLeeJY7CFmnl6Cyxm/oNHNFO6lySEGYHevyczhPyD6YMps4O1h0K9fpJGjqsx892ULee1kUOrK7iRrbNP7eNzOYIE+ObyriJrF9mRXR5HnF4xYa7wcVJyjwik6ZA9m1GK1MS7RChsB";
-
+export const HOST_TOKEN = "04AAAAAGkhreUADPZAFiEjiQZwCmUpYACz2UFpjg4ScptFa+iRn5v8e+Vvf7osT1TfB5qh0r2l5/EhG3vuxSx50mmsc3DY+3QVVYcLm9ZiAS81Qq+iqBtU5LST+s73QyQR37ucP9V6LANTow7YpNJcV7hC1geKcx+XcYiWLMo5TMdPfVyTF7CVeCEiJbfBJLo2gyxI1pE+E6TlTZ5ouTQMtPYP8Z7zEavYqUJgIdDp0g7NnsJbpPLbFmLdr+JI/T/siuz6lEY4+8erDJAB";
+export const VIEWER_TOKEN = "04AAAAAGkhrgcADNmVpnNQ0tVhpxO3rQC2SWLrGHlnhIWLqRHtIFl3o0UXLXg3cNs09RK3A8tCOLtKMmXVYCn89wJCqD/NKx4Qp0hNHnrHDuAIGKcEUWgvaV3hajyInhR6JPWVokiS7UnPi1uNF3wKAcSKJMJOrmITzqbz6b7H8oZv4Q2rOgsW8ssRZFDi5SAnbLk1eANzTe2lrtKbVk8yBEiIAagQJ5YJG2d3tdg3X//iJCEZR+Te45ScsxT+VSS7KdD4MprRCOMljHTkhLMB";
 export const HOST_USER_ID = "dibs-host";
 export const VIEWER_USER_ID = "dibs-viewer";
 
